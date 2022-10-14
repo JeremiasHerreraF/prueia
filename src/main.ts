587c1c177ee5b24c50aa137ed2fefab919c9e1af
JeremiasHerreraF <107441369+JeremiasHerreraF@users.noreply.github.com>
2022-10-14 11:27:50 -0300

    new changes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  const test = process.env.TEST 
+  const test = process.env.TEST || 'guau, funciona'; `¡Hola mundo desde NestJS! - ${test}`;
   console.log (test)
   await app.listen(port);
 }
